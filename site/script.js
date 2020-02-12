8220e17723cb8928093eaf8e14d37d5643a67757
moon791017 <moon791017@gmail.com>
2020-02-12 14:58:23 +0800

    dfdf

--- site/script.js
+++ site/script.js
@@ -42,10 +42,11 @@
         // Create a sequential model
         const model = tf.sequential();
         // Add a single hidden layer
-        model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }));
+        model.add(tf.layers.dense({ inputShape: [1], units: 20, useBias: true }));
 
         // Add an output layer
-        model.add(tf.layers.dense({ units: 1, useBias: true }));
+         model.add(tf.layers.dense({ units: 1, useBias: true }));
+        model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
 
         return model;
     }
@@ -92,6 +93,50 @@
         });
     }
 
+    function testModel(model, inputData, normalizationData) {
+        const {inputMax, inputMin, labelMin, labelMax} = normalizationData;  
+        
+        // Generate predictions for a uniform range of numbers between 0 and 1;
+        // We un-normalize the data by doing the inverse of the min-max scaling 
+        // that we did earlier.
+        const [xs, preds] = tf.tidy(() => {
+          
+          const xs = tf.linspace(0, 1, 100);      
+          const preds = model.predict(xs.reshape([100, 1]));      
+          
+          const unNormXs = xs
+            .mul(inputMax.sub(inputMin))
+            .add(inputMin);
+          
+          const unNormPreds = preds
+            .mul(labelMax.sub(labelMin))
+            .add(labelMin);
+          
+          // Un-normalize the data
+          return [unNormXs.dataSync(), unNormPreds.dataSync()];
+        });
+        
+       
+        const predictedPoints = Array.from(xs).map((val, i) => {
+          return {x: val, y: preds[i]}
+        });
+        
+        const originalPoints = inputData.map(d => ({
+          x: d.horsepower, y: d.mpg,
+        }));
+        
+        
+        tfvis.render.scatterplot(
+          {name: 'Model Predictions vs Original Data'}, 
+          {values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
+          {
+            xLabel: 'Horsepower',
+            yLabel: 'MPG',
+            height: 300
+          }
+        );
+      }
+
     async function trainModel(model, inputs, labels) {
         // Prepare the model for training.  
         model.compile({
@@ -127,6 +172,7 @@
         // Train the model  
         await trainModel(model, inputs, labels);
         console.log('Done Training');
+        testModel(model, data, tensorData);
 
     });
 
